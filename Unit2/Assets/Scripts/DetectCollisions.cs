71eac7a4fa02edd82fbbca7e6489615a99fc750c
P00min <718780@student.dusd.net>
2024-04-07 22:47:46 -0700

    1

--- Unit2/Assets/Scripts/DetectCollisions.cs
+++ Unit2/Assets/Scripts/DetectCollisions.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
+    private static int lives = 3;
+    private static int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-        Destroy(other.gameObject);
+        if (other.gameObject.name == "Player")
+        {
+            lives--;
+            Debug.Log("Lives: " + lives);
+            if (lives == 0)
+            {
+                Destroy(other.gameObject);
+                Debug.Log("Game Over!");
+            }
+        }
+        else if (other.gameObject.name != "Player")
+        {
+            score++;
+            Destroy(other.gameObject);
+            Debug.Log("Score: " + score);
+        }
     }
 }
